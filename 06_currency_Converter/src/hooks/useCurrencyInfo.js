c7d576f235dda8e58df75268ef4fc572b6150be8
shani-shil100x <shanishil100x@gmail.com>
2025-08-17 11:38:01 +0530

    add new project

--- 06_currency_Converter/src/hooks/useCurrencyInfo.js
+++ 06_currency_Converter/src/hooks/useCurrencyInfo.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
-    fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+    fetch(
+      `https://v6.exchangerate-api.com/v6/ff7f40c44ff6bb383af60905/latest/${currency}`
+    )
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
     console.table(data);
