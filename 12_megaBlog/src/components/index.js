015bb5b8fa154b7594631426a4aa86a61251b530
shani-shil100x <shanishil100x@gmail.com>
2025-08-24 08:34:36 +0530

    add something new

--- 12_megaBlog/src/components/index.js
+++ 12_megaBlog/src/components/index.js
@@ -4,6 +4,22 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
+import postCard from "./PostCard";
+import Login from "./Login";
+import Select from "./Select";
+import Signup from "./Signup";
+import RTE from "./RTE";
 
-
-export { Header, Footer, Container, Logo, LogoutBtn, Button };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  postCard,
+  Login,
+  Select,
+  Signup,
+  RTE,
+};
