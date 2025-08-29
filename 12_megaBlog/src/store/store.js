a0314fd6a6cd780c4d2c7e532f559b14f4c576c8
shani-shil100x <shanishil100x@gmail.com>
2025-08-29 14:14:35 +0530

    postSlice

--- 12_megaBlog/src/store/store.js
+++ 12_megaBlog/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import postSlice from "./postSlice";
+
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     // Todo: add postSlice
-    posts: postReducer,
+    posts: postSlice,
   },
 });
